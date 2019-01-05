ef1b677ca221fa6d39e6acf4febcf2aab0b28e72
Bob Tabor <bobtabor@outlook.com>
2019-01-05 13:12:36 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LiteralStringsTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // A literal string is -- literally -- a string 
+            // of individual characters, all strung together.
+
+            // You use a set of double-quotes to define a 
+            // literal string.
+            string myString = "Hi";
+
+            // What if you need to use double-quotes in your
+            // literal string?  Use an escape!
+            // myString = "He said \"Hi\".";
+
+            // What if you need to use a back-slash?
+            // myString = "Go to c:\\source\\repos";
+
+            // ... or use a verbatim string ...
+            // myString = @"Go to 
+            // c:\source\repos";
+
+            Console.WriteLine(myString);
+
+            // You can concatenate two strings together using 
+            // the concatenation operator:
+            // myString = "Bob" + " " + "Tabor";
+
+            // You can do this inside of a Console.WriteLine()
+            // Console.WriteLine("Hello" + " " + "World!");
+        }
+    }
+}
